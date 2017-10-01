9b9360d48c6f736a657c60ad0cdda75ea718925c
Joe DeCapo <joe@polka.cat>
2017-10-01 09:39:03 -0500

    Trying to get pygments to print with color

--- clowder/utility/clowder_yaml_printing.py
+++ clowder/utility/clowder_yaml_printing.py
@@ -1,16 +1,11 @@
 """Clowder yaml printing"""
 import os
 import sys
-import yaml
 from pygments import highlight
 from pygments.lexers.data import YamlLexer
 from pygments.formatters import get_formatter_by_name
 from clowder.utility.clowder_utilities import parse_yaml
-from clowder.utility.print_utilities import (
-    format_empty_yaml_error,
-    print_invalid_yaml_error,
-    print_open_file_error
-)
+from clowder.utility.print_utilities import print_open_file_error
 
 # Disable errors shown by pylint for no specified exception types
 # pylint: disable=W0702
@@ -40,13 +35,16 @@ def print_yaml(root_directory):
         if os.path.isfile(yaml_file):
             try:
                 with open(yaml_file) as file:
-                    parsed_yaml = yaml.safe_load(file)
-                    if parsed_yaml is None:
-                        print_invalid_yaml_error()
-                        print(format_empty_yaml_error(yaml_file))
+                    contents = file.read()
+                    terminal_formatter = get_formatter_by_name('terminal16m')
+                    outfile = os.path.join(root_directory, 'formatted_clowder.yaml')
+                    try:
+                        with open(outfile, 'w') as out_file:
+                            print(" - Save yaml to file")
+                            print(highlight(contents, YamlLexer(), terminal_formatter, outfile=out_file))
+                    except:
+                        # print_save_file_error(yaml_file)
                         sys.exit(1)
-                    terminal_formatter = get_formatter_by_name('terminal')
-                    print(highlight(parsed_yaml, YamlLexer(), terminal_formatter))
-            except:
+            except Exception as err:
                 print_open_file_error(yaml_file)
                 sys.exit(1)
